88eed27d686282f7707897c80b780118cab04373
Sailor Polaris <Chrisj1252@gmail.com>
2023-04-19 21:39:08 -0400

    This adds the pressure publisher

---
+++ src/graptor_pkg/src/pressure_sensor_publisher.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+
+import rospy
+import serial
+import time
+from std_msgs.msg import Int32
+
+#include "ros/ros.h"
+
+def talker():
+	ser = serial.Serial('/dev/ttyACM0', 115200)
+	time.sleep(0.1)
+	pressure_read = ser.readline()
+	pressure_d = pressure_read.decode()
+	pressure = pressure_d.rstrip()
+
+	pub = rospy.Publisher('Pressure', Int32, queue_size = 10)
+	rospy.init_node('pressure_node', anonymous = True)
+	rate = rospy.Rate(10)
+	while not rospy.is_shutdown():
+		rospy.loginfo(pressure)
+		pub.publish(pressure)
+		rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
