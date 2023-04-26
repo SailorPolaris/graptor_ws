2ce694bb5eefa84eb275a9d6fe2d45f865ab316b
Sailor Polaris <37009421+SailorPolaris@users.noreply.github.com>
2023-04-25 22:54:09 -0400

    Update pressure_sensor_publisher.py

--- src/graptor_pkg/src/pressure_sensor_publisher.py
+++ src/graptor_pkg/src/pressure_sensor_publisher.py
@@ -8,16 +8,22 @@ from std_msgs.msg import Int32
 #include "ros/ros.h"
 
 def talker():
-	ser = serial.Serial('/dev/ttyACM0', 115200)
-	time.sleep(0.1)
-	pressure_read = ser.readline()
-	pressure_d = pressure_read.decode()
-	pressure = pressure_d.rstrip()
-
+#	ser = serial.Serial('/dev/ttyACM0', 115200)
+#	time.sleep(0.1)
+#	pressure_read = ser.readline()
+#	pressure_d = pressure_read.decode()
+#	pressure = pressure_d.rstrip()
+#
 	pub = rospy.Publisher('Pressure', Int32, queue_size = 10)
 	rospy.init_node('pressure_node', anonymous = True)
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
+		ser = serial.Serial('/dev/ttyACM0', 115200)
+		time.sleep(0.1)
+		pressure_read = ser.readline()
+		pressure_d = pressure_read.decode()
+		pressure = pressure_d.rstrip()
+		
 		rospy.loginfo(pressure)
 		pub.publish(pressure)
 		rate.sleep()
